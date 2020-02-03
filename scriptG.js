1cc8ca818107cd798abf94558b84a668fbe7c731
Lucas Tavares <38914522+Tavaresl95@users.noreply.github.com>
2020-02-03 16:30:10 -0300

    Display de cor

--- scriptG.js
+++ scriptG.js
@@ -1,13 +1,24 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient")
+var body = document.getElementById("gradient");
 
 
 function setGradient() {
-    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")", 
+    verCor1(), verCor2();
+};
+
+function verCor1() {
+    document.getElementById("cor1").innerHTML = color1.value;
+};
+
+function verCor2() {
+    document.getElementById("cor2").innerHTML = color2.value;
 };
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+
